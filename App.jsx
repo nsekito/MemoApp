918da8e274e3a4010b4fd3a17ae856d5e7b9537e
Sekito Naoki <nsekito@gmail.com>
2021-10-12 01:17:37 +0900

    Miner fixes

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 import firebase from 'firebase';
@@ -20,6 +21,7 @@ if (firebase.apps.length === 0) {
 }
 
 const Stack = createStackNavigator();
+LogBox.ignoreLogs(['Setting a timer']);
 
 export default function App() {
   return (
